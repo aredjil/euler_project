479ca917d3761547a888dae1294476aa278406fc
aredjil <bellred499@gmail.com>
2025-02-10 13:56:49 +0100

    added counter

--- problem_930/src/main.cpp
+++ problem_930/src/main.cpp
@@ -32,7 +32,8 @@ int main(int argc, char **argv)
         {
             std::vector<int> bowls(n, 0);
             std::vector<int> balls(m, 0);
-            std::cout<<"Computing G("<<n<<","<<m<<")\n";
+            std::cout<<count<<"/121\n\n";
+            std::cout<<"Computing F("<<n<<","<<m<<")\n";
             double expected_steps = get_expected_steps(bowls, balls, n_steps);
 #pragma omp atomic
             total_sum += expected_steps;
